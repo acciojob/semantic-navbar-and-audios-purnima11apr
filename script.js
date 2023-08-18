17695d12f9a99c7262a35df8cb40af7aade56d2a
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-08-18 17:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 //your code here
-it('should display 3 random audios with correct sources and controls', () => {
-  cy.contains('3 random audios');
-  cy.get('audio').should($audios => {
-    expect($audios).to.have.length(3);
-    expect($audios).to.have.attr('controls');
+
     
-    const srcs = [];
-    $audios.each((i, el) => {
-      srcs.push(Cypress.$(el).find('source').attr('src'));
-    });
     
-    expect(srcs).to.deep.equal([
-      'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
-      'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
-      'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3'
-    ]);
-  });
-});
 
